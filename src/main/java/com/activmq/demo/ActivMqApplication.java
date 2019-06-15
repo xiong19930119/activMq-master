9a5807520a9e2b07061e003f09b287067feb2847
Pei WenXiong <664322256@qq.com>
2019-06-15 10:53:43 +0800

    first commit

---
+++ src/main/java/com/activmq/demo/ActivMqApplication.java
@@ -0,0 +1,13 @@
+package com.activmq.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivMqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActivMqApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/activmq/demo/TestList.java
@@ -0,0 +1,14 @@
+package com.activmq.demo;
+
+import org.junit.Test;
+
+public class TestList {
+
+    @Test
+    public void testList() {
+        long round = Math.round(-1.6);
+
+        System.out.println(round);
+    }
+
+}
